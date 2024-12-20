78ee34178c5420758c1df642b65f59a23c60de7c
Frederik Lauber <acc-github-frederik-lauber@flambda.de>
2024-12-20 23:33:43 +0100

    ADD: Day 9

--- src/09/main.rs
+++ src/09/main.rs
@@ -1,9 +1,14 @@
-use std::collections::{HashMap, HashSet};
+use std::cmp::min;
+use std::collections::{BTreeMap, HashMap, HashSet};
+use itertools::Itertools;
 
 fn main(){
-    let data = include_str!("example");
+    let data = include_str!("input");
     let mut files: HashMap<u32, HashSet<u32>> = HashMap::new();
-    let mut files_defragmented: HashMap<u32, HashSet<usize>> = HashMap::new();
+    let mut files_defragmented: HashMap<u32, HashSet<u32>> = HashMap::new();
+    let mut block2fileid: BTreeMap<u32, u32> = BTreeMap::new();
+    let mut block2fileid_start: BTreeMap<u32, String> = BTreeMap::new();
+
 
     let mut block_id = 0;
     let mut free_blocks : HashSet<u32> = HashSet::new();
@@ -13,33 +18,45 @@ fn main(){
         if index % 2 == 1{
             for _ in 0..val{
                 free_blocks.insert(block_id);
+                block2fileid_start.insert(block_id, ".".parse().unwrap());
                 block_id += 1;
             }
         } else {
             let file_index = (index /2) as u32;
             for _ in 0..val{
                 files.entry(file_index).or_default().insert(block_id);
+                block2fileid_start.insert(block_id, file_index.to_string());
                 block_id += 1;
             }
         }
     }
 
-    println!("{:?}", files);
     let biggest_file = *files.keys().max().expect("Foo");
 
-    'outer: for file_index in biggest_file..0{
-        while files[file_index].len() > 0 {
-            if free_blocks.len() == 0{
-                break 'outer;
-            }
-            let max_block = files[file_index].max();
+    for file_index in (0..=biggest_file).rev(){
+        while files[&file_index].len() > 0 {
 
+            let max_block = *files[&file_index].iter().max().expect("sdsds");
+            let new_block = if free_blocks.len() > 0{
+                let tmp = *free_blocks.iter().min().expect("sdsds");
+                free_blocks.remove(&tmp);
+                min(tmp, max_block)
+            }else{
+                max_block
+            };
 
-
+            let mut file_blocks = files.get_mut(&file_index).expect("sd");
+            file_blocks.remove(&max_block);
+            files_defragmented.entry(file_index).or_default().insert(new_block);
+            block2fileid.insert(new_block, file_index);
         }
-
+    }
+    let mut res:u128 = 0;
+    for (file_index, blocks) in files_defragmented.iter(){
+        for block in blocks.iter(){
+            res = res + (file_index * block) as u128;
+        }
     }
 
-
-
+    println!("Checksum: {}", res);
 }
